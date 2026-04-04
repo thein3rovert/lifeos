c35976331e89a9db1eee7878f48c19c61ae0ffc2
thein3rovert <danielolaibi@gmail.com>
2026-04-04 20:01:32 +0100

    chore: basic hello world

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 )
 
+//===============NOTE=============
+// go build
+// use curl for all api testing
 
 func main() {
 	_ = fmt.Sprint
@@ -15,16 +18,19 @@ func main() {
 	// Call http lib and handle function with this
 	// Go has the ability to take function as paramater
 
-	// lWhen /'api is called -> handler -> function
+	// When /'api is called -> handler -> function
 	http.HandleFunc("/api", apiHandler)
 
-	fmt.Println("Starting sever at port 8080 ...")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting sever at port 5837 ...")
+	if err := http.ListenAndServe(":5837", nil); err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
 
 }
 
 // * httpRquest points to a location where user requewst abd param are persent
 // -> user provided data
+// http.ResponseWriter -> backend writes it resposne
 func apiHandler(writeResponse http.ResponseWriter, userRequest *http.Request) {
-
+	fmt.Fprint(writeResponse, "HELLO THEIN3ROVERT")
 }
