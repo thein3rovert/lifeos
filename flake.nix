c2fd353d589239d4b1709d8f4c69696c92e3ee2f
thein3rovert <danielolaibi@gmail.com>
2026-04-04 19:33:08 +0100

    chore: chore: add flake support

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_23
+            gopls
+            gotools
+            go-tools
+          ];
+
+          shellHook = ''
+            echo "Go $(go version)"
+          '';
+        };
+      }
+    );
+}
