d403a4832aa545132c579df69c5d23c40939da8d
thein3rovert <danielolaibi@gmail.com>
2026-04-06 12:12:13 +0100

    chore: template render successful

--- internal/handler/photo.go
+++ internal/handler/photo.go
@@ -1,7 +1,17 @@
 package handler
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func Photos(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("photo page"))
+	// w.Write([]byte("photo page"))
+
+	// Integrate template
+	tmpl := template.Must(template.ParseFiles(
+		"templates/base.html",
+		"templates/photos.html",
+	))
+	tmpl.ExecuteTemplate(w, "base", nil)
 }
--- internal/handler/skills.go
+++ internal/handler/skills.go
@@ -1,7 +1,17 @@
 package handler
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func Skills(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("skills page"))
+	// w.Write([]byte("skills page"))
+
+	// Integrate template
+	tmpl := template.Must(template.ParseFiles(
+		"templates/base.html",
+		"templates/skills.html",
+	))
+	tmpl.ExecuteTemplate(w, "base", nil)
 }
