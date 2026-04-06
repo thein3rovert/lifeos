796cd8363370f73c724014e9af11dd951d7d6b5f
thein3rovert <danielolaibi@gmail.com>
2026-04-06 19:11:25 +0100

    chore: handle to return all list of photos

--- internal/handler/photo.go
+++ internal/handler/photo.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/thein3rovert/lifeos/internal/store"
 )
 
 func Photos(w http.ResponseWriter, r *http.Request) {
@@ -14,4 +16,20 @@ func Photos(w http.ResponseWriter, r *http.Request) {
 		"templates/photos.html",
 	))
 	tmpl.ExecuteTemplate(w, "base", nil)
+}
+
+// List returns a handler that fetches all photos and renders the template
+func ListPhotos(s store.SQLiteStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		photos, err := s.ListPhotos()
+		if err != nil {
+			http.Error(w, "could not load photos", http.StatusInternalServerError)
+		}
+
+		tmpl := template.Must(template.ParseFiles(
+			"templates/base.html",
+			"templates/photos.html",
+		))
+		tmpl.ExecuteTemplate(w, "base", photos)
+	}
 }
