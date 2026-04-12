2998e19c5e0010db9fcbb6378e0903467b6fd844
thein3rovert <danielolaibi@gmail.com>
2026-04-12 23:08:01 +0100

    chore: add tags interface

--- internal/store/store.go
+++ internal/store/store.go
@@ -5,4 +5,10 @@ import "github.com/thein3rovert/lifeos/internal/model"
 type Store interface {
 	SavePhoto(photo *model.Photo) error
 	ListPhotos() ([]model.Photo, error)
+
+	// Tags
+	SaveTag(name string) (int64, error)
+	AddTagToPhoto(photoID, tagID int64) error
+	ListTags() ([]model.Tag, error)
+	GetPhotoTags(photoID int64) ([]model.Tag, error)
 }
