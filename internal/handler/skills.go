13ded7ede9d787e672c7fd49cdba8a2c2c67357e
thein3rovert <danielolaibi@gmail.com>
2026-04-18 18:05:03 +0100

    chore: add goldmark for rending html, convert skills to rendered html

--- internal/handler/skills.go
+++ internal/handler/skills.go
@@ -1,11 +1,24 @@
 package handler
 
 import (
+	"bytes"
 	"html/template"
 	"net/http"
 
 	"github.com/thein3rovert/lifeos/internal/store"
+	"github.com/yuin/goldmark"
 )
+
+// TODO: Mocw to a dedicated page later
+type SkillViewData struct {
+	ID 		string
+	Title string
+	Format string
+	Content string
+	HTMLContent string
+	UpdatedAt string
+}
+
 
 func Skills(w http.ResponseWriter, r *http.Request) {
 	// w.Write([]byte("skills page"))
@@ -52,10 +65,27 @@ func GetSkill(s store.SkillStore) http.HandlerFunc {
 				return
 			}
 
+			// Convert the get markdown to html using bytes for conversion
+			// and goldmark for rendering
+			var buf bytes.Buffer
+			if err := goldmark.Convert([]byte(skill.Content), &buf); err != nil {
+				// If conversion fails, use raw content
+				buf.WriteString(skill.Content)
+			}
+
+			data := SkillViewData {
+				ID: skill.ID,
+				Title: skill.Title,
+				Format: skill.Format,
+				Content: skill.Content,
+				HTMLContent: buf.String(),
+				UpdatedAt: skill.UpdatedAt.String(),
+			}
+
 			tmpl := template.Must(template.ParseFiles(
 				"templates/base.html",
 				"templates/skill.html",
 			))
-			tmpl.ExecuteTemplate(w, "base", skill)
+			tmpl.ExecuteTemplate(w, "base", data)
 	}
 }
