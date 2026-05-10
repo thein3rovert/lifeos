0a89850266fa6ee8e1165f8b5c1d4b14416c7e5a
thein3rovert <danielolaibi@gmail.com>
2026-05-10 15:24:25 +0100

    chore: allow patch method in middleware

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -33,7 +33,7 @@ func CORS(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, HX-Request")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
